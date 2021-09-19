0fab49e0e0e9ebcffee56bef095fe8273e986dc2
SpiralOutDotEu <spiralout.eu@gmail.com>
2021-09-19 18:23:21 +0300

    Fixed Homework 1

--- code/week02/src/Week02/Homework1.hs
+++ code/week02/src/Week02/Homework1.hs
@@ -32,23 +32,29 @@ import           Text.Printf          (printf)
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are equal!
 mkValidator :: () -> (Bool, Bool) -> ScriptContext -> Bool
-mkValidator _ _ _ = True -- FIX ME!
+mkValidator _ (b, c) _ = traceIfFalse "Wrong Redeemer: Two booleans are not equal" $ b == c  -- FIXED!
 
 data Typed
 instance Scripts.ValidatorTypes Typed where
--- Implement the instance!
+    type instance DatumType Typed = ()
+    type instance RedeemerType Typed = (Bool, Bool) --FIXED!
 
 typedValidator :: Scripts.TypedValidator Typed
-typedValidator = undefined -- FIX ME!
+-- typedValidator = undefined -- FIXED!
+typedValidator = Scripts.mkTypedValidator @Typed
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @() @(Bool, Bool)  -- FIXED!
 
 validator :: Validator
-validator = undefined -- FIX ME!
+validator = Scripts.validatorScript typedValidator -- FIXED!
 
 valHash :: Ledger.ValidatorHash
-valHash = undefined -- FIX ME!
+valHash = Scripts.validatorHash typedValidator -- FIXED!
 
 scrAddress :: Ledger.Address
-scrAddress = undefined -- FIX ME!
+scrAddress = scriptAddress validator -- FIXED!
 
 type GiftSchema =
             Endpoint "give" Integer
